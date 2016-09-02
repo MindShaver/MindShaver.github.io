fb6175db8fd43aaff509a580f8387aec60113f3e
Robert Livingston <rll@bu.edu>
2016-09-02 09:31:59 -0500

    Initial Set-Up
    Pushing initial index, css, and js files.

---
+++ js/mainJS.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  
+  
+            $( "#accordion" ).accordion({
+          collapsible: true
+        
+      });
+  
+  
+  $('.mainLogo').hide().fadeIn("slow");
+  
+        $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+      });
+  
+  $(".events img").on('mouseenter',function(){
+    $(this).fadeTo(500, 0.5);
+  });
+  
+  $(".events img").on('mouseleave',function(){
+    $(this).fadeTo(500, 1);
+  });
+  
+ 
+    });
+
